680c70fef7a485d93c614838f9b80efea8eced6f
Santiago Diaz <gotzdiaz@gmail.com>
2022-08-18 21:31:06 -0300

    commit desafioNavbar, no se renderiza en el DOM

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-// import logo from './logo.svg';
 import './App.css';
-import navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+const App = () => {
+
   
   
-  return (navbar())
+  return (
+    <div>
+      <h1>App</h1>
+      
+        <Navbar/>
+    </div>
   
-}
+  )
+};
 
 export default App;
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+
+const Navbar = () => {
+    <div>
+        <h1> Navbar</h1>
+    </div>
+    
+    //  <header id="header">
+    //     <nav className="menu">
+    //         <ul class="nav-menu">
+    //             <li><a className="active" href="index.html">Home</a></li>
+    //             <li><a href="pages/tienda.html">Tienda</a></li>
+    //             <li><a href="pages/iniciarsesion.html">Iniciar Sesion</a></li>
+    //             <li><a href="pages/registrarse.html">Registrarse</a></li>
+    //         </ul>
+    //     </nav>
+    //  </header>
+
+} 
+
+
+export default Navbar;
