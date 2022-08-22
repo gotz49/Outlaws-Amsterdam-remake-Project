67502093423bbb1a6dc3286a5ed3e359751ffaf3
Santiago Diaz <gotzdiaz@gmail.com>
2022-08-22 18:57:34 -0300

    [

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 
 const Navbar = () => {
-
-     <header id="header">
-        <nav className="menu">
-            <ul class="nav-menu">
-                <li><a className="active" href="index.html">Home</a></li>
-                <li><a href="pages/tienda.html">Tienda</a></li>
-                <li><a href="pages/iniciarsesion.html">Iniciar Sesion</a></li>
-                <li><a href="pages/registrarse.html">Registrarse</a></li>
-            </ul>
-        </nav>
-     </header>
+    return (
+    <header id="header">
+    <nav className="menu">
+        <ul class="nav-menu">
+            <li><a className="active" href="index.html">Home</a></li>
+            <li><a href="pages/tienda.html">Tienda</a></li>
+            <li><a href="pages/iniciarsesion.html">Iniciar Sesion</a></li>
+            <li><a href="pages/registrarse.html">Registrarse</a></li>
+        </ul>
+    </nav>
+    </header>
+    )
+     
 
 } 
 
